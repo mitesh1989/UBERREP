137bfdac5bcc2eb3afe17e37a79c12f04f7e8fb4
mitesh1989 <odesk.mitesh@gmail.com>
2014-08-14 00:30:55 +0530

    add retailer

---
+++ UBERREP/Retailer.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UBERREP
+{
+    public partial class Retailer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UsersList1.UserType = BusinessLayer.Users.UserTypes.Retailer;
+
+            if (!Page.IsPostBack)
+            {
+                NameValueCollection spData = new NameValueCollection();
+                spData.Add("type", Convert.ToString((int)UsersList1.UserType));
+                UsersList1.UserList = BusinessLayer.Users.UserManager.GetUsers(spData);
+                UsersList1.BindData();
+            }
+        }
+    }
+}
