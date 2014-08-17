665fed2ebfaa600bb20f8984e1a15e9e611bed79
mitesh1989 <odesk.mitesh@gmail.com>
2014-08-16 21:14:59 -0700

    salesList

---
+++ UBERREP/Controls/SalesList.ascx
@@ -0,0 +1,114 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SalesList.ascx.cs" Inherits="UBERREP.Controls.SalesList" %>
+
+<div class="portlet-body">
+    <script type="text/javascript">
+
+        function print_page() {
+
+            window.print();
+
+        }
+
+    </script>
+
+
+
+    <asp:Repeater ID="UserListRPT" runat="server" >
+        <HeaderTemplate>
+            <div class="clearfix">
+                <div class="btn-group">
+                    <asp:Button ID="sample_editable_1_new" runat="server" class="btn red" CommandName="Insert" Text="Add New" />
+                </div>
+                <div class="btn-group pull-right">
+                    <button class="btn dropdown-toggle" data-toggle="dropdown">
+                        Tools <i class="icon-angle-down"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#" onclick="javascript :print_page();">Print</a></li>
+                        <li>
+                            <asp:LinkButton ID="BTNSaveToPDF" runat="server" Text="Save as PDF" OnClick="BTNSaveToPDF_Click" /></li>
+                        <li>
+                            <asp:LinkButton ID="BTNSaveToExcel" runat="server" Text="Export to Excel" OnClick="BTNSaveToExcel_Click" /></li>
+                    </ul>
+                </div>
+            </div>
+            <table class="table table-striped table-hover table-bordered" id="sample_editable_1">
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th style="width: 20% !IMPORTANT">Full Name</th>
+                        <th style="width: 20% !IMPORTANT">Points</th>
+                        <th style="width: 20% !IMPORTANT">Notes</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <asp:Panel ID="insertPnl" runat="server" Visible="false">
+                    <tr class="">
+                        <td>
+                            <asp:TextBox ID="txt_ins_Username" class="m-wrap small fixBig" runat="server" />
+                            
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txt_ins_Fullname" class="m-wrap small fixSmall" runat="server" />
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txt_ins_Points" class="m-wrap small fixSmall" runat="server" />
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txt_ins_Notes" class="m-wrap small fixSmall" runat="server" />
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="LinkButton1" runat="server" Text="Save" CommandName="Save" />
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="LinkButton2" runat="server" Text="Cancel" CommandName="InsertCancel" />
+                        </td>
+                    </tr>
+                </asp:Panel>
+        </HeaderTemplate>
+
+        <ItemTemplate>
+
+            <tr class="">
+                <td style="width: 160px;">
+                    <asp:Label ID="lbl_pass" Visible="false" runat="server" Text="<%#(((UBERREP.BusinessLayer.Users.User)Container.DataItem).Password).ToString() %>"></asp:Label>
+                      
+                    <asp:Label ID="lbl_Email" Visible="false" runat="server" Text="<%#(((UBERREP.BusinessLayer.Users.User)Container.DataItem).Email).ToString() %>"></asp:Label>
+                    <asp:Literal ID="Lit_usename" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Username%>' />
+                    <asp:TextBox ID="TXT_username" class="m-wrap small fixBig" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Username%>' Visible="false" />
+
+                </td>
+                <td>
+                    <asp:Literal ID="lit_FullName" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Name%>' />
+                    <asp:TextBox ID="txt_FullName" class="m-wrap small fixSmall" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Name%>' Visible="false" />
+                </td>
+                <td>
+                    <asp:Literal ID="lit_Point" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Points%>' />
+                    <asp:TextBox ID="txt_Point" class="m-wrap small fixSmall" Columns="4" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Points%>' Visible="false" />
+                </td>
+                <td>
+                    <asp:Literal ID="lit_Notes" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Remarks%>' />
+                    <asp:TextBox ID="txt_Notes" class="m-wrap small fixSmall" runat="server" Text='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).Remarks%>' Visible="false" />
+                </td>
+                <td>
+                    <asp:LinkButton ID="lnk_Edit" runat="server" Text="Edit" CommandName="Edit" />
+                    <asp:LinkButton ID="lnk_Update" Visible="false" runat="server" Text="Save" CommandName="Update" CommandArgument='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).ID%>' />
+
+                </td>
+                <td>
+                    <asp:LinkButton ID="lnk_Cancel" Visible="false" runat="server" Text="Cancel" CommandName="Cancel" />
+                    <asp:LinkButton ID="lnk_delete" runat="server" Text="Delete" CommandArgument='<%#((UBERREP.BusinessLayer.Users.User)Container.DataItem).ID%>' CommandName="Delete" OnClientClick='javascript:return confirm("Are you sure you want to delete?")' />
+
+                </td>
+            </tr>
+
+        </ItemTemplate>
+
+
+        <FooterTemplate>
+            </tbody>
+            </table>
+        </FooterTemplate>
+    </asp:Repeater>
+</div>
---
+++ UBERREP/Controls/SalesList.ascx.cs
@@ -0,0 +1,211 @@
+﻿using BusinessLayer.Common;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace UBERREP.Controls
+{
+    public partial class SalesList : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) BindReapeater();
+        }
+
+
+
+
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+
+            base.OnPreRender(e);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+            this.Load += new System.EventHandler(this.Page_Load);
+            this.UserListRPT.ItemCommand += UserListRPT_ItemCommand;
+
+        }
+
+
+        public void BindReapeater()
+        {
+
+            NameValueCollection spData = new NameValueCollection();
+            spData.Add("type",Convert.ToString((int)BusinessLayer.Users.UserTypes.Sales));
+            UserListRPT.DataSource = BusinessLayer.Users.UserManager.GetUsers(spData);
+            UserListRPT.DataBind();
+            //this.BindData();
+        }
+         protected void BTNSaveToPDF_Click(object sender, EventArgs e)
+        {
+            Response.AddHeader("content-disposition", "attachment;filename=Export.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+            HtmlForm frm = new HtmlForm();
+            UserListRPT.Parent.Controls.Add(frm);
+            frm.Attributes["runat"] = "server";
+            frm.Controls.Add(UserListRPT);
+            frm.RenderControl(hw);
+            StringReader sr = new StringReader(sw.ToString());
+            Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+            HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+            PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+            pdfDoc.Open();
+            htmlparser.Parse(sr);
+            pdfDoc.Close();
+            Response.Write(pdfDoc);
+            Response.End();
+        }
+
+
+        private void UserListRPT_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+            switch (e.CommandName)
+            {
+                case "Delete":
+                    {
+                        BusinessLayer.Users.User obj = new BusinessLayer.Users.User();
+                        obj.ID = Convert.ToInt16(e.CommandArgument);
+                        BusinessLayer.Users.UserManager.Delete(obj);
+                        this.BindReapeater();
+                        ShowMessage("Record Deleted Successfully");
+                        break;
+                    }
+                case "Insert":
+                    {
+                        e.Item.FindControl("insertPnl").Visible = true;
+                        break;
+                    }
+                case "InsertCancel":
+                    {
+                        e.Item.FindControl("insertPnl").Visible = false;
+                        break;
+                    }
+                case "Edit":
+                    {
+                        e.Item.FindControl("lnk_Update").Visible = true;
+                        e.Item.FindControl("lnk_delete").Visible = false;
+                        e.Item.FindControl("lnk_Cancel").Visible = true;
+                        e.Item.FindControl("lnk_Edit").Visible = false;
+                        e.Item.FindControl("Lit_usename").Visible = false;
+                        e.Item.FindControl("TXT_username").Visible = true;
+                        e.Item.FindControl("lit_Point").Visible = false;
+                        e.Item.FindControl("txt_Point").Visible = true;
+                        e.Item.FindControl("lit_Notes").Visible = false;
+                        e.Item.FindControl("txt_Notes").Visible = true;
+                        e.Item.FindControl("lit_FullName").Visible = false;
+                        e.Item.FindControl("txt_FullName").Visible = true;
+                        break;
+                    }
+                case "Cancel":
+                    {
+                        e.Item.FindControl("lnk_Update").Visible = false;
+                        e.Item.FindControl("lnk_delete").Visible = true;
+                        e.Item.FindControl("lnk_Cancel").Visible = false;
+                        e.Item.FindControl("lnk_Edit").Visible = true;
+                        e.Item.FindControl("Lit_usename").Visible = true;
+                        e.Item.FindControl("TXT_username").Visible = false;
+                        e.Item.FindControl("lit_Point").Visible = true;
+                        e.Item.FindControl("txt_Point").Visible = false;
+                        e.Item.FindControl("lit_Notes").Visible = true;
+                        e.Item.FindControl("txt_Notes").Visible = false;
+                        e.Item.FindControl("lit_FullName").Visible = true;
+                        e.Item.FindControl("txt_FullName").Visible = false;
+                        break;
+                    }
+                case "Update":
+                    {
+                        BusinessLayer.Users.User obj = new BusinessLayer.Users.User();//((List<BusinessLayer.Users.User>)((Repeater)source).DataSource)[e.Item.ItemIndex];
+                        obj.ID = Convert.ToInt16(e.CommandArgument);
+                        obj.Type = BusinessLayer.Users.UserTypes.Sales;
+                        obj.Password = ((System.Web.UI.WebControls.Label)(e.Item.FindControl("lbl_pass"))).Text;
+                        obj.Email = ((System.Web.UI.WebControls.Label)(e.Item.FindControl("lbl_Email"))).Text == string.Empty ? "NAN" : ((System.Web.UI.WebControls.Label)(e.Item.FindControl("lbl_Email"))).Text;
+                        obj.Username = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("TXT_username"))).Text;
+                        obj.Name = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_FullName"))).Text;
+                        //obj.RecordNumber = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_Point"))).Text;
+                        //obj.Type = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_Notes"))).Text;
+
+                        obj.Remarks = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_Notes"))).Text;
+                        obj.Points = Convert.ToDecimal(((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_Point"))).Text);
+
+                        BusinessLayer.Users.UserManager.Update(obj);
+                        
+                        this.BindReapeater();
+                        ShowMessage("Record Updated Successfully");
+                        break;
+                    }
+                case "Save":
+                    {
+                        BusinessLayer.Users.User retObj = new BusinessLayer.Users.User();
+                        //  retObj.Email = retObj.Username = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("TXT_username"))).Text;
+                        retObj.Name = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_ins_Username"))).Text;
+                        retObj.Password = "123456";
+                        retObj.Username = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_ins_Fullname"))).Text;
+                        retObj.Type = BusinessLayer.Users.UserTypes.Sales;
+                        retObj.Status = Status.Active;
+                        retObj.Remarks = ((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_ins_Notes"))).Text;
+                        retObj.Points = Convert.ToDecimal(((System.Web.UI.WebControls.TextBox)(e.Item.FindControl("txt_ins_Points"))).Text);
+                        retObj.Email = "NAN";
+                        BusinessLayer.Users.UserManager.Create(retObj);
+
+                        
+                        this.BindReapeater();
+                        ShowMessage("Record Inserted Successfully");
+                        break;
+                    }
+            }
+
+        }
+        
+
+
+        #region AlertMessage
+        public void ShowMessage(string message) { ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('" + message + "')", true); }
+        #endregion
+
+        protected void BTNSaveToExcel_Click(object sender, EventArgs e)
+        {
+            string filename = "DownloadTest.xls";
+            System.IO.StringWriter tw = new System.IO.StringWriter();
+            System.Web.UI.HtmlTextWriter hw = new System.Web.UI.HtmlTextWriter(tw);
+            UserListRPT.RenderControl(hw);
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + filename + "");
+            this.EnableViewState = false;
+            Response.Write(tw.ToString());
+            Response.End();
+        }
+
+        protected void UserListRPT_ItemCommand1(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+
+    }
+}
